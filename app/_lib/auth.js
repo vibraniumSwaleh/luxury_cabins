2eb1aac495657c81be1c6c264772ba717b686043
vibraniumSwaleh <hus.swaleh@yahoo.com>
2026-01-12 21:26:18 +0000

    add new user on signIn

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { createGuest, getGuest } from './data-service';
 
 const authConfig = {
   providers: [
@@ -11,6 +12,23 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingUser = await getGuest(user.email);
+
+        if (!existingUser)
+          await createGuest({ email: user.email, fullName: user.name });
+
+        return true;
+      } catch (error) {
+        false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
--- app/account/page.js
+++ app/account/page.js
@@ -8,6 +8,8 @@ export default async function Page() {
   const { user } = await auth();
   const firstName = user?.name.split(' ')[0];
 
+  console.log(user);
+
   return (
     <h2 className='font-semibold text-2xl text-accent-400 mb-4'>
       Welcome, {firstName}
